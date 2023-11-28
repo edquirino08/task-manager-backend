a97d14bad5d7b3d60194ead82cf69acae8eaadbd
Eduardo Quirino <eduardo.araujo.quirino@hotmail.com>
2023-11-28 16:25:50 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,11 @@
 const userService = require('../services/userService');
+const baseServices = require('../services/baseServices');
 
 const login = async (req, res) => {
     try {
         const access = await userService.login(req.body.email, req.body.password);
-        userService.baseServices.createLog(req.user.id, '/login');
+        const user = await baseServices.findUserByToken(access.token);
+        userService.baseServices.createLog(user.id, '/login');
         return res.status(200).json(access);
     } catch (err) {
         userService.baseServices.createError(req.headers.token, `Error /login: ${err.message}`);
